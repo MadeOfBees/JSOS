76d011e2c6af1ab4a7a4850c66be1b040b989642
Bee <xronsredstone@gmail.com>
2022-12-21 18:01:15 -0500

    SOS inputs

--- src/Components/DrawBoard.js
+++ src/Components/DrawBoard.js
@@ -1,7 +1,47 @@
 import * as React from 'react';
 
 
-const DisplayBoard = ({ board, classes, difficulty }) => {
- 
-        }
-        
+const DisplayBoard = ({ totalSquares }) => {
+  const [board, setBoard] = React.useState([]);
+  const [isPlayersTurn, setIsPlayersTurn] = React.useState(true);
+
+  const DrawBoard = () => {
+    const boardArray = [];
+    for (let i = 0; i < totalSquares; i++) {
+      boardArray.push("");
+    }
+    setBoard(boardArray); 
+  }
+
+  React.useEffect(() => DrawBoard(), [totalSquares]);
+
+  const handleInput = (e) => {
+    if (isPlayersTurn) {
+      if (e.target.value === 's' || e.target.value === 'o' || e.target.value === 'S' || e.target.value === 'O') {
+        e.target.disabled = true;
+        e.target.value = e.target.value.toUpperCase();
+        setIsPlayersTurn(false);
+        CPUTurn();
+      }
+    }
+  }  
+
+  const CPUTurn = () => {
+
+  }
+  
+
+
+  return (
+    <div>
+      {board.map((item) => {
+        return (
+          <input key={item} style={{ display: 'inline-block', width: '50px', height: '50px', border: '1px solid black' }} onChange={handleInput}></input>
+        )
+      }
+      )}
+    </div>
+  );
+}
+
+export default DisplayBoard;
--- src/Pages/MainApp.js
+++ src/Pages/MainApp.js
@@ -10,15 +10,38 @@ export default function MainApp() {
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
     const style = { position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'background.paper', border: '2px solid #000', boxShadow: 24, p: 4, };
+    const [userInput, setUserInput] = React.useState(0);
+    const [totalUserSquares, setTotalUserSquares] = React.useState(userInput);
+
+    const setUserInputFunction = (e) => {
+        const uInput = e.target.value.trim();
+        if (isNaN(uInput)) return;
+        else if (uInput < 3) return;
+        else if (uInput > 100) return;
+        else if (uInput === "") return;
+        else setUserInput(uInput);
+    }
+
+    const takeUserInput = () => {
+        setTotalUserSquares(userInput);
+        handleClose();
+    }
 
     return (
         <div >
-            <Box sx={{ width: '100%' }}>
-              <DrawBoard />
+            <Box sx={{ flexGrow: 2}} style={{ marginTop: '20%' }}>
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    {totalUserSquares ? <DrawBoard totalSquares={totalUserSquares} /> : null}
+                </div>
+                <Grid container spacing={2} justifyContent="center" alignItems="center" style={{ marginTop: '5%' }}>
+                        <Button variant="contained" onClick={handleOpen}>Launch modal</Button>
+                </Grid>
+
             </Box>
             <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
                 <Box sx={style}>
-                    <p>Modal content</p>
+                    <input type="number" onChange={setUserInputFunction} />
+                    <Button variant="contained" onClick={takeUserInput}>Submit</Button>
                 </Box>
             </Modal>
         </div>
