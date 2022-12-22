85fdf6ca5795a30e95123838c52076aae01ed9db
Bee <xronsredstone@gmail.com>
2022-12-21 23:17:42 -0500

    a solid bass

--- src/Components/DrawBoard.js
+++ src/Components/DrawBoard.js
@@ -1,46 +1,91 @@
 import * as React from 'react';
-
-
-const DisplayBoard = ({ totalSquares }) => {
+import { Button, Grid, Stack, Modal, Box } from '@mui/material';
+const DisplayBoard = ({ totalSquares, refreshNum }) => {
   const [board, setBoard] = React.useState([]);
   const [isPlayersTurn, setIsPlayersTurn] = React.useState(true);
-
-  const DrawBoard = () => {
+  const [modal, setModal] = React.useState("");
+  const [open, setOpen] = React.useState(false);
+  const style = { position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'background.paper', border: '2px solid #000', boxShadow: 24, p: 4, };
+  const handleClose = () => setOpen(false);
+  const primary = "info";
+  const secondary = "warning";
+  const drawBoard = () => {
+    setOpen(false);
     const boardArray = [];
     for (let i = 0; i < totalSquares; i++) {
-      boardArray.push("");
+      boardArray.push(0);
     }
-    setBoard(boardArray); 
+    setBoard(boardArray);
+  }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  React.useEffect(() => drawBoard(), [refreshNum]);
+  const handleInput = (e, index) => {
+    const newBoard = [...board];
+    newBoard[index] = e.target.innerText;
+    setBoard(newBoard);
+    checkWinStatus(newBoard);
+    setIsPlayersTurn(false);
+    cpuTurn(newBoard);
+  }
+  const checkIfTie = (newBoard) => newBoard.join('').includes(0) ? "continue" : "tie";
+  const checkIfWin = (newBoard) => newBoard.join('').includes('SOS') ? (isPlayersTurn ? "win" : "loss") : "continue";
+  const checkWinStatus = (newBoard) => {
+    if (checkIfWin(newBoard) === "win") {endGame("You win!");}
+    if (checkIfWin(newBoard) === "loss") {endGame("You lose!");}
+    if (checkIfTie(newBoard) === "tie") {endGame("It's a tie!");}
   }
 
-  React.useEffect(() => DrawBoard(), [totalSquares]);
-
-  const handleInput = (e) => {
-    if (isPlayersTurn) {
-      if (e.target.value === 's' || e.target.value === 'o' || e.target.value === 'S' || e.target.value === 'O') {
-        e.target.disabled = true;
-        e.target.value = e.target.value.toUpperCase();
-        setIsPlayersTurn(false);
-        CPUTurn();
-      }
-    }
-  }  
-
-  const CPUTurn = () => {
-
+  const endGame = (input) => {
+    setModal(input);
+    setOpen(true);
   }
-  
 
+  const cpuTurn = (newBoard) => {
+    const emptyIndex = newBoard.indexOf(0);
+    newBoard[emptyIndex] = 'O';
+    setBoard(newBoard);
+    checkWinStatus(newBoard);
+    setIsPlayersTurn(true);
+  }
 
   return (
-    <div>
-      {board.map((item) => {
-        return (
-          <input key={item} style={{ display: 'inline-block', width: '50px', height: '50px', border: '1px solid black' }} onChange={handleInput}></input>
-        )
+    <Grid container spacing={1} justifyContent="center">
+      {board.map((item, index) => {
+        if (item === 'S') {
+          return (
+            <Grid item xs={1} key={index}>
+              <Button variant="contained" color={primary} onClick={(e) => handleInput(e, index)}>S</Button>
+            </Grid>
+          )
+        }
+        if (item === 'O') {
+          return (
+            <Grid item xs={1} key={index}>
+              <Button variant="contained" color={secondary} onClick={(e) => handleInput(e, index)}>O</Button>
+            </Grid>
+          )
+        }
+        else {
+          return (
+            <Grid item xs={1} key={index}>
+              <Stack>
+                <Button variant="contained" color={primary} onClick={(e) => handleInput(e, index)}>S</Button>
+                <Button variant="contained" color={secondary} onClick={(e) => handleInput(e, index)}>O</Button>
+              </Stack>
+            </Grid>
+          )
+        }
       }
       )}
-    </div>
+        <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+          <Box sx={style}>
+            <Stack spacing={2}>
+              <h2 id="modal-modal-title">{modal}</h2>
+              <Button variant="contained" onClick={drawBoard}>Play Again</Button>
+            </Stack>
+          </Box>
+        </Modal>
+    </Grid>
   );
 }
 
--- src/Pages/MainApp.js
+++ src/Pages/MainApp.js
@@ -4,44 +4,51 @@ import Button from '@mui/material/Button';
 import { Grid } from '@mui/material';
 import DrawBoard from '../Components/DrawBoard';
 import Modal from '@mui/material/Modal';
+import Slider from 'rc-slider';
+import {Stack} from '@mui/system';
+import 'rc-slider/assets/index.css';
 
 export default function MainApp() {
+    const ohHiMark = 15;
+    const marks = {};
+    for (let i = 3; i <= ohHiMark; i++) {marks[i] = i;}
+    const [hasStarted, setHasStarted] = React.useState(false);
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
     const style = { position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'background.paper', border: '2px solid #000', boxShadow: 24, p: 4, };
-    const [userInput, setUserInput] = React.useState(0);
-    const [totalUserSquares, setTotalUserSquares] = React.useState(userInput);
+    const [userInput, setUserInput] = React.useState(3);
+    const [totalUserSquares, setTotalUserSquares] = React.useState(3);
+    const [refreshNum, setRefreshNum] = React.useState(0);
 
     const setUserInputFunction = (e) => {
-        const uInput = e.target.value.trim();
-        if (isNaN(uInput)) return;
-        else if (uInput < 3) return;
-        else if (uInput > 100) return;
-        else if (uInput === "") return;
-        else setUserInput(uInput);
+        if (e < 3) return;
+        else setUserInput(e);
     }
 
     const takeUserInput = () => {
+        setRefreshNum(refreshNum + 1);
         setTotalUserSquares(userInput);
+        setHasStarted(true);
         handleClose();
     }
 
     return (
         <div >
-            <Box sx={{ flexGrow: 2}} style={{ marginTop: '20%' }}>
+            <Box sx={{ flexGrow: 2 }} style={{ marginTop: '20%' }}>
                 <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                    {totalUserSquares ? <DrawBoard totalSquares={totalUserSquares} /> : null}
+                    {totalUserSquares ? <DrawBoard totalSquares={totalUserSquares} refreshNum={refreshNum} /> : null}
                 </div>
                 <Grid container spacing={2} justifyContent="center" alignItems="center" style={{ marginTop: '5%' }}>
-                        <Button variant="contained" onClick={handleOpen}>Launch modal</Button>
+                    <Button variant="contained" onClick={handleOpen}>{hasStarted ? "New Game" : "Start Game"}</Button>
                 </Grid>
-
             </Box>
             <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
                 <Box sx={style}>
-                    <input type="number" onChange={setUserInputFunction} />
-                    <Button variant="contained" onClick={takeUserInput}>Submit</Button>
+                    <Stack>
+                    <Slider min={3} max={ohHiMark} marks={marks} step={1} defaultValue={2} onChange={setUserInputFunction}/>
+                    <Button variant="contained" onClick={takeUserInput} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '20%' }}>Submit</Button>
+                    </Stack>
                 </Box>
             </Modal>
         </div>
